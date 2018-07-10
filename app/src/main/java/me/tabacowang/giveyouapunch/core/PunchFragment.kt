8eac6a1fce0d9f8b8e77693a6563fa993e203362
7a6ac0 <jack.7517@gmail.com>
2018-07-10 22:33:55 +0800

    Add punch_fragment.xml

--- app/src/main/java/me/tabacowang/giveyouapunch/core/PunchFragment.kt
+++ app/src/main/java/me/tabacowang/giveyouapunch/core/PunchFragment.kt
@@ -2,12 +2,20 @@ package me.tabacowang.giveyouapunch.core
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import me.tabacowang.giveyouapunch.databinding.PunchFragmentBinding
 
 class PunchFragment : Fragment() {
+    private lateinit var punchFragmentBinding: PunchFragmentBinding
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        punchFragmentBinding = PunchFragmentBinding.inflate(inflater, container, false).apply {
+            viewmodel = (activity as PunchActivity).obtainViewModel()
+        }
+        setHasOptionsMenu(true)
+        return punchFragmentBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/me/tabacowang/giveyouapunch/core/PunchViewModel.kt
+++ app/src/main/java/me/tabacowang/giveyouapunch/core/PunchViewModel.kt
@@ -2,6 +2,9 @@ package me.tabacowang.giveyouapunch.core
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
+import android.graphics.drawable.Drawable
 import me.tabacowang.giveyouapunch.data.PunchRepository
 import javax.inject.Inject
 
@@ -10,5 +13,9 @@ class PunchViewModel
         context: Application,
         private val punchRepository: PunchRepository
 ) : AndroidViewModel(context) {
+
+    val noPunchLabel = ObservableField<String>()
+    val noPunchIconRes = ObservableField<Drawable>()
+    val empty = ObservableBoolean(true)
 
 }
